2a1c6dc73fcca6ee3cf1d3a21f4b033c00b71a4c
mkydy <202292735@qq.com>
2018-10-25 18:16:41 +0800

    枚举测试

---
+++ apkjartest/src/apkjartest/TestEnumImpl.java
@@ -0,0 +1,11 @@
+package apkjartest;
+
+import apkjartest.TestEnum.Color;
+
+public class TestEnumImpl {
+
+	public static void main(String[] args) {
+		//调用get方法
+		System.out.println(Color.ROLE.getUserName());
+	}
+}
