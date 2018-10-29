00561c5aaa477e2f09c7162191c4cfb5ed9e0699
mkydy <202292735@qq.com>
2018-10-29 16:56:16 +0800

    emmmm

--- apkjartest/src/apkjartest/Generic.java
+++ apkjartest/src/apkjartest/Generic.java
@@ -1,5 +1,8 @@
 package apkjartest;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 import apkjartest.entity.Book;
 
 /**
@@ -11,7 +14,31 @@ public class Generic {
 
 	public static void main(String[] args) {
 		Book<String> book = new Book<String>("English");
-		System.out.println("book:"+book.getData());
+		//System.out.println("book:"+book.getData());
+		testClass(book);
 	}
 	
+	public static void testDiff() {
+		Book<String> book = new Book<String>("English");
+		Book<Integer> age = new Book<Integer>(755);
+		System.out.println("book class"+book.getClass());
+		System.out.println("age class"+age.getClass());
+	}
+	
+	public static <E> void testClass(E e) {
+		//getClass().getGenericSuperclass()返回表示此 Class 所表示的实体（类、接口、基本类型或 void）的直接超类的 Type
+		Type genType = e.getClass().getGenericSuperclass();
+		 System.out.println(genType);
+		 
+		 //然后将其转换ParameterizedType getActualTypeArguments()返回表示此类型实际类型参数的 Type 对象的数组
+		 Type[] param = ((ParameterizedType) genType).getActualTypeArguments();
+		 System.out.println(param);
+		 
+		 //getSimpleName 得到类的简写名称
+		 String className = ((Class<?>) param[0]).getSimpleName();
+		 System.out.println(className);
+		 
+		 StringBuffer hql = new StringBuffer("from "+className+" t1 where 1=1");
+		 System.out.println(hql);
+	}
 }
